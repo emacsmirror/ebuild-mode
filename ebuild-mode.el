64fb8c42999dcd957172e93e41d033bf35f5f1f2
Christian Faulhammer <opfer@gentoo.org>
2007-03-16 10:20:28 +0000

    adding version number
    svn path=/emacs-extra/ebuild-mode/; revision=48

--- ebuild-mode.el
+++ ebuild-mode.el
@@ -6,7 +6,8 @@
 ;; Author: Diego Pettenò <flameeyes@gentoo.org>
 ;; Author: Christian Faulhammer <opfer@gentoo.org>
 ;; Keywords: convenience
-
+;; version: 1.4
+;;
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 2, or (at your option)
