fd78f253a6a652086a1b3bfe6c5f95a72631e9b3
Christian Faulhammer <opfer@gentoo.org>
2007-10-11 06:55:47 +0000

    (ebuild-mode-commands-toolchain-funcs): Added missing functions from marienz' ebuild-mode
    svn path=/emacs-extra/gentoo-syntax/; revision=699

--- gentoo-syntax.el
+++ gentoo-syntax.el
@@ -98,7 +98,9 @@ A formfeed is not considered whitespace by this function."
 (defvar ebuild-mode-commands-toolchain-funcs
   '("tc-getPROG" "tc-getAR" "tc-getAS" "tc-getCC" "tc-getCPP" "tc-getCXX"
     "tc-getLD" "tc-getSTRIP" "tc-getNM" "tc-getRANLIB" "tc-getF77" "tc-getF90"
-    "tc-getFORTRAN" "tc-getGCJ" "tc-getBUILD_CC"))
+    "tc-getFORTRAN" "tc-getGCJ" "tc-getBUILD_CC" "tc-export" "tc-is-cross-compiler"
+    "tc-ninja_magic_to_arch" "tc-arch-kernel" "tc-arch" "tc-endian" "gcc-fullversion"
+    "gcc-version" "gcc-major-version" "gcc-minor-version" "gcc-micro-version"))
 
 (defvar ebuild-mode-commands-toolchain
   '("is_crosscompile" "tc_version_is_at_least" "gcc_get_s_dir" "gentoo_urls" "get_gcc_src_uri"
