82b4701a4143f4cf83455603660ef21bde3d8627
Ulrich Müller <ulm@gentoo.org>
2007-11-10 17:43:13 +0000

    Also set sh-shell.
    svn path=/emacs-extra/gentoo-syntax/; revision=737

--- gentoo-syntax.el
+++ gentoo-syntax.el
@@ -596,6 +596,7 @@ A formfeed is not considered whitespace by this function."
   (add-hook 'write-contents-hooks 'delete-trailing-whitespace t t)
   (add-hook 'write-contents-hooks 'ebuild-mode-tabify t t)
   (setq sh-shell-file "/bin/bash")
+  (setq sh-shell 'bash)
   (setq tab-width 4)
   (setq indent-tabs-mode t)
   ;; run user-defined hooks
