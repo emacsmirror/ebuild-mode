e5b874cc4cb2ebe4d49b2fc644e119deb04537cf
Ulrich Müller <ulm@gentoo.org>
2008-03-30 10:07:24 +0000

    Don't use match-string-no-properties since it doesn't exist in XEmacs 21.4.
    svn path=/gentoo-syntax/; revision=1036

--- gentoo-syntax.el
+++ gentoo-syntax.el
@@ -168,7 +168,10 @@ A formfeed is not considered whitespace by this function."
 	   (mapcar (lambda (s)
 		     (string-match "^\\([-~]?\\)\\(.*\\)" s)
 		     (cons (match-string 2 s) (match-string 1 s)))
-		   (split-string (match-string-no-properties 1)))))))
+		   (split-string
+		    ;;(match-string-no-properties 1) ; not in XEmacs 21.4
+		    (buffer-substring-no-properties (match-beginning 1)
+						    (match-end 1))))))))
 
 (defun ebuild-mode-put-keywords (kw &optional noerror)
   (save-excursion
