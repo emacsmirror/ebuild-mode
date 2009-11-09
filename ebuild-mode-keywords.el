17b32fee6d3c1d09bdbc704ff33dd0c0496a295b
Christian Faulhammer <fauli@gentoo.org>
2009-11-09 11:45:16 +0000

    gentoo-syntax: function java-ant_rewrite-bootclasspath added
    svn path=/gentoo-syntax/; revision=1453

--- ebuild-mode-keywords.el
+++ ebuild-mode-keywords.el
@@ -363,7 +363,8 @@
 
 (defvar ebuild-mode-keywords-java-ant-2
   '(("java-ant_bsfix_files" "java-ant_bsfix_one" "java-ant_rewrite-classpath"
-     "java-ant_ignore-system-classes" "java-ant_xml-rewrite")
+     "java-ant_ignore-system-classes" "java-ant_xml-rewrite"
+     "java-ant_rewrite-bootclasspath")
     font-lock-type-face))
 
 (defvar ebuild-mode-keywords-java-utils-2
