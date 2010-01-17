4046bac41c718d4956c090b881f6d62809fc2743
Christian Faulhammer <fauli@gentoo.org>
2010-01-17 21:56:57 +0000

    gentoo-syntax: (ebuild-mode-keywords-linux-info) Add get_makefile_extract_function function
    svn path=/gentoo-syntax/; revision=1503

--- ebuild-mode-keywords.el
+++ ebuild-mode-keywords.el
@@ -223,7 +223,7 @@
      "linux_chkconfig_builtin" "linux_chkconfig_string" "kernel_is"
      "get_localversion" "get_version" "get_running_version"
      "check_kernel_built" "check_modules_supported" "check_extra_config"
-     "check_zlibinflate" "linux-info_get_any_version")
+     "check_zlibinflate" "linux-info_get_any_version" "get_makefile_extract_function")
     font-lock-type-face))
 
 (defvar ebuild-mode-keywords-linux-mod
