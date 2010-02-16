95ad2c13deb392dd029bc337bb26d26441afb81c
Ulrich Müller <ulm@gentoo.org>
2010-02-16 10:32:13 +0000

    Add svn keywords.
    svn path=/gentoo-syntax/; revision=1518

--- ebuild-mode-keywords.el
+++ ebuild-mode-keywords.el
@@ -9,6 +9,8 @@
 ;; Maintainer: <emacs@gentoo.org>
 ;; Keywords: languages
 
+;; $Id$
+
 ;; This file is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 2 of the License, or
