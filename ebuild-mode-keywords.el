1d3767b659e81a4310f1c457f0a726cdb62ebe3a
Christian Faulhammer <fauli@gentoo.org>
2010-07-13 10:04:39 +0000

    gentoo-syntax: qt_nolibx11() function
    svn path=/gentoo-syntax/; revision=1553

--- ebuild-mode-keywords.el
+++ ebuild-mode-keywords.el
@@ -275,7 +275,8 @@
      "build_directories" "install_directories" "install_qconfigs"
      "generate_qconfigs" "skip_qmake_build_patch"
      "skip_project_generation_patch" "symlink_binaries_to_buildtree"
-     "fix_library_files" "qt_use" "qt_mkspecs_dir" "qt_assistant_cleanup")
+     "fix_library_files" "qt_use" "qt_mkspecs_dir" "qt_assistant_cleanup"
+     "qt_nolibx11")
     font-lock-type-face))
 
 (defvar ebuild-mode-keywords-kde4-functions
