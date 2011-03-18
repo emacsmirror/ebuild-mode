68bb07004dbdc4d0ff1708e29aa8cbf1bbada33d
Christian Faulhammer <fauli@gentoo.org>
2011-03-18 22:13:27 +0000

    gentoo-syntax (ebuild-mode-keywords-vmware-bundle): Add vmware-bundle eclass.
    svn path=/gentoo-syntax/; revision=1648

--- ebuild-mode-keywords.el
+++ ebuild-mode-keywords.el
@@ -639,6 +639,11 @@
      "version_format_string")
     font-lock-type-face))
 
+(defvar ebuild-mode-keywords-vmware-bundle
+  '(("vmware-bundle_extract-bundle-component" "vmware-bundle_extract-component"
+     "")
+    font-lock-type-face))
+
 ;; Local Variables:
 ;; coding: utf-8
 ;; fill-column: 78
