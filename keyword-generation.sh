a9fc92356cbdf14b5f4d64adf0b3ba745d91a3cb
Christian Faulhammer <fauli@gentoo.org>
2011-12-30 01:32:10 +0000

    gentoo-syntax: Filter leechcraft
    svn path=/gentoo-syntax/; revision=1704

--- keyword-generation.sh
+++ keyword-generation.sh
@@ -13,7 +13,8 @@ ECLASSES=$(cd $(portageq portdir)/eclass/;ls *.eclass)
 
 # Obsolete eclasses or ones which contain no functions
 for filter in git bash-completion gems ruby qt4 php-ext-pecl-r1 \
-    php-ext-source-r1 gnome.org gnustep-2 java-mvn-src kde4-meta-pkg
+    php-ext-source-r1 gnome.org gnustep-2 java-mvn-src kde4-meta-pkg \
+    leechcraft
 do
     ECLASSES=${ECLASSES//${filter}.eclass/}
 done
