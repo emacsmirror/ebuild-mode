e28ed4c1e0e2c74afb5593ce65a3830a1e0a41c7
Ulrich Müller <ulm@gentoo.org>
2017-01-15 19:44:23 +0100

    Update copyright years.

--- ebuild-mode.el
+++ ebuild-mode.el
@@ -1,6 +1,6 @@
 ;;; ebuild-mode.el --- edit Gentoo ebuild and eclass files
 
-;; Copyright 2006-2016 Gentoo Foundation
+;; Copyright 2006-2017 Gentoo Foundation
 
 ;; Author: Matthew Kennedy <mkennedy@gentoo.org>
 ;;	Diego Pettenò <flameeyes@gentoo.org>
