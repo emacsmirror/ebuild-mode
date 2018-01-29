b1f5a4dcee53d15e3433fbc0e87c14a95f0d69b1
Ulrich Müller <ulm@gentoo.org>
2018-01-29 11:05:04 +0100

    Version 1.42 released.

--- ebuild-mode.el
+++ ebuild-mode.el
@@ -7,7 +7,7 @@
 ;;	Christian Faulhammer <fauli@gentoo.org>
 ;;	Ulrich Müller <ulm@gentoo.org>
 ;; Maintainer: <emacs@gentoo.org>
-;; Version: 1.41
+;; Version: 1.42
 ;; Keywords: languages, processes
 
 ;; This file is free software: you can redistribute it and/or modify
