52150f46a71126c1b94488f3b59473544acd0137
Ulrich Müller <ulm@gentoo.org>
2018-06-05 21:43:32 +0200

    Fix line breaks in GLEP footer.
    * glep-mode.el (glep-mode-insert-skeleton): Fold lines as in the
    standard GLEP footer.

--- glep-mode.el
+++ glep-mode.el
@@ -366,9 +366,9 @@ References
 Copyright
 =========
 
-This work is licensed under the Creative Commons
-Attribution-ShareAlike 3.0 Unported License.  To view a copy of this
-license, visit http://creativecommons.org/licenses/by-sa/3.0/.
+This work is licensed under the Creative Commons Attribution-ShareAlike 3.0
+Unported License.  To view a copy of this license, visit
+http://creativecommons.org/licenses/by-sa/3.0/.
 ")
 
 ;; rst-mode already uses the following C-c C-<letter> keys: aclrt
