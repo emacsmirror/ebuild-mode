emacsmirror/ebuild-mode

commit 077c42069ffe50eb89eb4a079e1a70d9cb9c633f

Fix a byte-compile warning
* ebuild-mode.el (ebuild-repo-mode): Fix byte-compile warning
in XEmacs.

Signed-off-by: Ulrich Müller <ulm@gentoo.org>

ebuild-mode.el
@@ -647,7 +647,8 @@ and `all-completions' for details."
     (add-hook 'write-contents-hooks 'ebuild-repo-mode-before-save t t))
   (setq fill-column 72)
   (setq tab-width 4)
-  (when (derived-mode-p 'nxml-mode)
+  (when (and (not (featurep 'xemacs))
+	     (derived-mode-p 'nxml-mode))
     (set (make-local-variable 'nxml-child-indent)
 	 (if ebuild-mode-xml-indent-tabs 4 2))
     (set (make-local-variable 'nxml-attribute-indent)

commit 227bb44a8c02612ca9d5d55643c0b68be3bbbb13

Really fix the XEmacs byte-compile warning
Fixes: 077c42069ffe50eb89eb4a079e1a70d9cb9c633f
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

ebuild-mode.el
@@ -647,12 +647,11 @@ and `all-completions' for details."
     (add-hook 'write-contents-hooks 'ebuild-repo-mode-before-save t t))
   (setq fill-column 72)
   (setq tab-width 4)
-  (when (and (not (featurep 'xemacs))
-	     (derived-mode-p 'nxml-mode))
-    (set (make-local-variable 'nxml-child-indent)
-	 (if ebuild-mode-xml-indent-tabs 4 2))
-    (set (make-local-variable 'nxml-attribute-indent)
-	 (* 2 nxml-child-indent))
+  (when (derived-mode-p 'nxml-mode)
+    (eval-when-compile (ignore-errors (require 'nxml-mode)))
+    (let ((indent (if ebuild-mode-xml-indent-tabs 4 2)))
+      (set (make-local-variable 'nxml-child-indent) indent)
+      (set (make-local-variable 'nxml-attribute-indent) (* 2 indent)))
     (setq indent-tabs-mode ebuild-mode-xml-indent-tabs)))
 
 ;;;###autoload