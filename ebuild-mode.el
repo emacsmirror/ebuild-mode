529191d3b6691401f9612e761e1c5448f9359857
Ulrich Müller <ulm@gentoo.org>
2024-01-08 17:03:22 +0100

    Version 1.70 released
    Signed-off-by: Ulrich Müller <ulm@gentoo.org>

--- ebuild-mode.el
+++ ebuild-mode.el
@@ -7,7 +7,7 @@
 ;;	Christian Faulhammer <fauli@gentoo.org>
 ;;	Ulrich Müller <ulm@gentoo.org>
 ;; Maintainer: <emacs@gentoo.org>
-;; Version: 1.69
+;; Version: 1.70
 ;; Keywords: languages, processes
 
 ;; This file is free software: you can redistribute it and/or modify
